e9eca37a9f8ce1875b42658fcbe0374665691c10
kylo666 <32401843+kylo666@users.noreply.github.com>
2017-10-29 21:10:11 +0100

    Add files via upload

---
+++ zadanie_10_3/js/script.js
@@ -0,0 +1,15 @@
+var carouselList = $("#carousel ul");
+
+function changeSlide() {
+	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+}
+
+function moveFirstSlide() {
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	lastItem.before(firstItem);
+	carouselList.css({marginLeft:0});
+}
+
+setInterval(changeSlide, 3000);
+
